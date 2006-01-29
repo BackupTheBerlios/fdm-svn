bb8638d4186f28176a637343ab955f91ee2ccdea
mikejj <mikejj@d2e01259-7909-0410-af3d-b89895206819>
2006-01-29 12:48:28 +0000

    Removed need to 2 copies of the fdm-res pictures (by changing location of the .vcproj and .rc files

--- trunk/Batch-Files/View-CVS-Stuff.bat
+++ trunk/Batch-Files/View-CVS-Stuff.bat
@@ -6,6 +6,7 @@ cd..
 attrib +h /S /D Batch-Files > nul
 attrib +h /S /D Fdm-Client > nul
 attrib +h /S /D Fdm-Windows > nul
+attrib +h /S /D Fdm-Res > nul
 attrib +h /S /D Other-Projects > nul
 attrib +h /S /D .svn > nul
 attrib +h /S /D _svn > nul
@@ -14,8 +15,8 @@ attrib +h /S /D app > nul
 attrib +h /S *.ncb > nul
 attrib +h /S *.suo > nul
 attrib +h /S *.aps > nul
+attrib +h Fdm-*.* > nul
 attrib +h Changelog-Fdm.txt > nul
-attrib +h Fdm-Example.xml > nul
 attrib +h License-Fdm.txt > nul
 attrib +h *.rar > nul
 rd /s /q Temp > nul
@@ -31,13 +32,14 @@ rd /s /q Temp > nul
 attrib -h /S /D Batch-Files > nul
 attrib -h /S /D Fdm-Client > nul
 attrib -h /S /D Fdm-Windows > nul
+attrib -h /S /D Fdm-Res > nul
 attrib -h /S /D Other-Projects > nul
 attrib -h /S /D vc7 > nul
 attrib -h /S /D app > nul
 attrib -h /S *.ncb > nul
 attrib -h /S *.suo > nul
 attrib -h /S *.aps > nul
+attrib -h Fdm-*.* > nul
 attrib -h Changelog-Fdm.txt > nul
-attrib -h Fdm-Example.xml > nul
 attrib -h License-Fdm.txt > nul
 attrib -h *.rar > nul
--- trunk/Fdm-Windows/resource.h
+++ trunk/Fdm-Windows/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by Fdm-Windows.rc
+// Used by C:\fdm\client\fdm\Fdm-Windows.rc
 //
 #define IDD_FDM_DIALOG                  103
 #define IDB_FDM_TOOLBAR20               104
