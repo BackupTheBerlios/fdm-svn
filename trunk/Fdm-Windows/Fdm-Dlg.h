039969f06069bd1b12baa5ad96d46752c235fc45
carraya <carraya@d2e01259-7909-0410-af3d-b89895206819>
2006-01-29 17:25:18 +0000

    colour and stuff for fdm settings

--- trunk/Fdm-Windows/Fdm-Dlg.h
+++ trunk/Fdm-Windows/Fdm-Dlg.h
@@ -1,4 +1,5 @@
 #include "Resource.h"
+#include "../Fdm-Client/dcplusplus-rips/Fdm-Version.h"
 
 #if !defined(FDM_DLG_H)
 #define FDM_DLG_H
@@ -12,7 +13,6 @@ class FdmDlg : public CDialogImpl<FdmDlg>
 {
 public:
 
-
 	enum { IDD = IDD_FDM_DIALOG };
 
 	FdmDlg() { };
@@ -20,11 +20,21 @@ public:
 
 	BEGIN_MSG_MAP(FdmDlg)
 		MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+		MESSAGE_HANDLER(WM_CTLCOLORDLG, OnCtrlColor)
+		MESSAGE_HANDLER(WM_CTLCOLORSTATIC, OnCtrlColor)
+		MESSAGE_HANDLER(WM_CTLCOLORBTN, OnCtrlColor)
 		COMMAND_ID_HANDLER(IDOK, OnCloseCmd)
 		COMMAND_ID_HANDLER(IDCANCEL, OnCloseCmd)
 	END_MSG_MAP()
 
+	LRESULT OnCtrlColor(UINT, WPARAM wParam, LPARAM, BOOL&) {
+		SetBkMode((HDC)wParam, TRANSPARENT); // transparent background needed for the rest to draw
+		SetTextColor((HDC)wParam, RGB(0, 21, 180)); // blueish text
+		return (LRESULT)AtlGetStockBrush(WHITE_BRUSH); // white brush (background drawn after bk mode)
+	}
+
 	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/) {
+		SetDlgItemText(IDC_FDM_SETTINGS_TEXT, _T("Fdm settings text test. Fdm version: ") _T(FDMVERSIONSTRING));
 		CenterWindow(GetParent());
 		return TRUE;
 	}
--- trunk/Fdm-Windows/resource.h
+++ trunk/Fdm-Windows/resource.h
@@ -1,10 +1,11 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by C:\fdm\client\fdm\Fdm-Windows.rc
+// Used by j:\DC++Sources\Fdm\Fdm-Windows.rc
 //
 #define IDD_FDM_DIALOG                  103
 #define IDB_FDM_TOOLBAR20               104
 #define IDB_FDM_TOOLBAR20_HOT           105
+#define IDC_FDM_SETTINGS_TEXT           1001
 
 // Next default values for new objects
 // 
@@ -12,7 +13,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
